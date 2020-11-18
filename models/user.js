e00d487e02fa63120d6ed2a81d76b132a68e336a
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-11-17 19:05:53 -0700

    added code to create and post activity

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcryptjs");
 
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define(
     "User",
     {
@@ -19,8 +19,25 @@ module.exports = function(sequelize, DataTypes) {
       password: {
         type: DataTypes.STRING,
         allowNull: false
-      }
+      },
+      // username: {
+      //   type: DataTypes.STRING,
+      //   defaultValue: 'Anonymous',
+      //   allowNull: false
+      // },
+      // gender: {
+      //   type: DataTypes.STRING,
+      //   allowNull: false
+      // },
+      // phone: {
+      //   allowNull: false,
+      //   type: DataTypes.STRING,
+      //   validate: {
+      //     not: ['[a-z]', 'i']
+      //   }
+      
     },
+
     {
       // This forces any default 'User' to exclude the password when we query them;
       // this way we don't expose even a hashed password
@@ -37,12 +54,12 @@ module.exports = function(sequelize, DataTypes) {
     }
   );
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
@@ -50,12 +67,15 @@ module.exports = function(sequelize, DataTypes) {
     );
   });
 
-  User.associate = function(models) {
+  User.associate = function (models) {
     // Associating User with Posts
     // When an User is deleted, also delete any associated Posts
     User.hasMany(models.Post, {
       onDelete: "cascade"
-    });
+    }),
+      User.hasMany(models.Activity, {
+        onDelete: "cascade"
+      });
   };
 
   return User;
