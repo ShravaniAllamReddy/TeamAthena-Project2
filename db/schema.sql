f316312f40ef1b7deb1998b931fdd536422434e1
Moniqueemh <mohernandez9992@yahoo.com>
2020-11-23 17:31:12 -0800

    added fonts

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
 DROP DATABASE IF EXISTS project_2_local;
 CREATE DATABASE project_2_local;
-
-DROP DATABASE IF EXISTS project_2_test;
-CREATE DATABASE project_2_test;
+use project_2_local;
+select *
+from users;
+select *
+from activities;
